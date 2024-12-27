c62cda9463aaa4110c72ead960aebb67a1ae1de3
nsazhi <n190275@inbox.ru>
2024-12-27 13:38:03 +0400

    27/12/24 add admin - task4

--- gameshop/task1/admin.py
+++ gameshop/task1/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    list_filter = ('cost', 'size')
+    search_fields = ('title',)
+    list_per_page = 20
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('balance', 'age')
+    search_fields = ('name',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
